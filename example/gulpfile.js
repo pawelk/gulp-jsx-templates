91c504699cdd57ba6ee44793f6f8e74c9a2196eb
p.knapik <pawel.knapik@gmail.com>
2016-03-09 11:52:45 +0100

    added html beautify to the example project

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -3,6 +3,8 @@ var jsx = require('../index.js');
 var rename = require('gulp-rename');
 var fs = require('fs');
 
+var htmlbeautify = require('gulp-html-beautify');
+
 gulp.task('jsx', function() {
 
     var templatedata = JSON.parse(fs.readFileSync('./src/data.json'));
@@ -14,6 +16,7 @@ gulp.task('jsx', function() {
 	
     gulp.src('./src/pages/*.jsx')
 		.pipe( jsx( templatedata, jsxoptions ))
+		.pipe(htmlbeautify({ indentSize : 2 }))
 		.pipe( rename( function(path){
 			path.extname = '.html';
 		}))
